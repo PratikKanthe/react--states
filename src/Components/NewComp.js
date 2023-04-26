78fd073f07ed7c2ee9a37ef8d071ce5c160127ac
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-26 22:53:24 +0530

    added: images

--- src/Components/NewComp.js
+++ src/Components/NewComp.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react'
+import bellA from "./BellA.png"
+// import bellB from "./bellB.png"
 
 class NewComp extends Component {
     styles={
         fontstyle: "italic",
         color: "#4c1d95"
       }
+      ButtonChange = () =>{
+        this.setState({
+            message: "Hit the bell icon to never miss an update",
+            sub: "Subscribed"
+        })
+      }
     constructor(props) {
       super(props)
     
       this.state = {
-         message: "Please subscribe to the channel :)"
+         message: "Please subscribe to the channel :)",
+         sub: "Subscribe",
+         imageURL: bellA
       }
     }
   render() {
     return (
       <div className='App'>
         <h1 style={this.styles}>{this.state.message}</h1>
+        <button onClick={this.ButtonChange}>{this.state.sub}</button>
+        <img src={this.state.imageURL}  alt=''/>
       </div>
     )
   }
