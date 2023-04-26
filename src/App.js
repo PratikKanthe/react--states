f342fca29a64337a69ef2fdb51b8b36821d2a743
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-04-26 22:15:36 +0530

    update: styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import './App.css';
 
 class App extends React.Component {
+  styles={
+    fontstyle: "bold",
+    color: "teal"
+  }
  render() {
   return(
     <div className="App">
-      <h1>React States</h1>
+      <h1 style={this.styles}>React States</h1>
     </div>
   );
  }
